5ee18d9f9dda6f93276af54a0e4940fe7bb7d3e4
Evin Ugur <evinoog96@gmail.com>
2013-01-05 12:43:20 -0500

    Hardcoded field constants...

---
+++ FieldElements.h
@@ -0,0 +1,16 @@
+//basic (and probably temporary - best to move them to Subsystems, or maybe keep things here... who knows) field element properties 
+#ifndef FIELD_ELEMENTS_H
+#define FIELD_ELEMENTS_H
+
+#define PI = 3.14	//TODO replace this with the appropriate call in the Math library
+
+//pyramid constants
+#define PYRAMID_FIRST_LEVEL_IN = 30
+#define PYRAMID_SECOND_LEVEL_IN = 60
+#define PYRAMID_THIRD_LEVEL_IN = 90
+#define PYRAMID_DIAMATER_IN = 5/PI
+
+//target constants
+#define TAPE_WIDTH_IN = 4.5
+
+#endif
