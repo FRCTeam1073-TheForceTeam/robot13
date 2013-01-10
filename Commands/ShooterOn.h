9cddb559f8b1e4aa4376aa01be9c73deb76ac9d9
KC <kccowan@yahoo.com>
2013-01-09 20:46:58 -0500

    Framework With Shooter
    Designed by rookies

---
+++ Commands/ShooterOn.h
@@ -0,0 +1,21 @@
+#ifndef SHOOTERON_H
+#define SHOOTERON_H
+#include "Commands/Subsystem.h"
+#include "../Robot.h"
+
+/**
+ *
+ *
+ * @author ExampleAuthor
+ */
+class ShooterOn: public Command {
+public:
+	ShooterOn();
+	virtual void Initialize();
+	virtual void Execute();
+	virtual bool IsFinished();
+	virtual void End();
+	virtual void Interrupted();
+};
+
+#endif
