796ff5b40cae903adc0fba50f6f230fea9a20eb8
Evin Ugur <evinoog96@gmail.com>
2013-01-15 07:39:19 -0500

    Turns out you can't concat strings in C++ with +

--- Subsystems/DriveTrain.cpp
+++ Subsystems/DriveTrain.cpp
@@ -38,4 +38,4 @@ void DriveTrain::Move(){
 float DriveTrain::GetDriveVoltage(bool left){return left ? leftDriveMotor->GetBusVoltage() : rightDriveMotor->GetBusVoltage();}
 float DriveTrain::GetDriveCurrent(bool left){return left ? leftDriveMotor->GetOutputCurrent() : rightDriveMotor->GetOutputCurrent();}
 float DriveTrain::GetDriveSpeed(bool left){return left ? leftDriveMotor->GetSpeed() : rightDriveMotor->GetSpeed();}
-float DriveTrain::GetGyroX() {return driveTrainGyro->GetAngle();}
+float DriveTrain::GetGyroAngle() {return driveTrainGyro->GetAngle();}
