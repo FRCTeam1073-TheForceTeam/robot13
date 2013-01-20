c577296d8a7fddf21e888353a9ce6ccc86e95315
Evin Ugur <evinoog96@gmail.com>
2013-01-20 13:56:22 -0500

    Implemented WDD If Zero'd out method bodies

--- Commands/WriteDriveData.cpp
+++ Commands/WriteDriveData.cpp
@@ -4,31 +4,36 @@ const char * WriteDriveData::name = "DriveData.csv";WriteDriveData::WriteDriveDa
 	//Hack diagnostic command, do not actually require the drivetrain!
 	//Requires(Robot::driveTrain);
 	Requires(Robot::shooter);
+#if 0
 	file = fopen(name, "w+");
 	fprintf(file, "Left Encoder Speed, Left Encoder Voltage, Left Encoder Current, " 
 		"Right Encoder Speed, Right Encoder Voltage, Right Encoder Current, "
 		"Shooter Encoder Speed, Shooter Encoder Voltage, Shooter Encoder Current, "
 		"Battery Voltage, Gyro Angle\n");
 	fclose(file);
+#endif
 }
 void WriteDriveData::Initialize() {
+#if 0
 	file = fopen(name, "w+");
 	printf("Starting dump to:\n");
 	puts(name);
+#endif
 }
-//void WriteDriveData::Execute() {
-	//fprintf(file, "%f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n",
-//		Robot::driveTrain->GetDriveSpeed(true), Robot::driveTrain->GetDriveBusVoltage(true),
-//		Robot::driveTrain->GetDriveCurrent(true), Robot::driveTrain->GetDriveSpeed(false),
-//		Robot::driveTrain->GetDriveBusVoltage(false), Robot::driveTrain->GetDriveCurrent(false),
-//		Robot::shooter->GetEncoderSpeed(), Robot::shooter->GetVoltage(), Robot::shooter->GetCurrent(),
-//		DriverStation::GetInstance()->GetBatteryVoltage(), Robot::driveTrain->GetGyroAngle());
-			
-//}
-bool WriteDriveData::IsFinished() {return false;}
+void WriteDriveData::Execute() {
+#if 0
+	fprintf(file, "%f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n",
+		Robot::driveTrain->GetDriveSpeed(true), Robot::driveTrain->GetDriveBusVoltage(true),
+		Robot::driveTrain->GetDriveCurrent(true), Robot::driveTrain->GetDriveSpeed(false),
+		Robot::driveTrain->GetDriveBusVoltage(false), Robot::driveTrain->GetDriveCurrent(false),
+		Robot::shooter->GetEncoderSpeed(), Robot::shooter->GetVoltage(), Robot::shooter->GetCurrent(),
+		DriverStation::GetInstance()->GetBatteryVoltage(), Robot::driveTrain->GetGyroAngle());
+#endif			
+}
+bool WriteDriveData::IsFinished() {return true;}	//TODO: make this right
 void WriteDriveData::End() {Finished();}
 void WriteDriveData::Interrupted() {Finished();}
 void WriteDriveData::Finished(){
 	puts("Dump finished");
-	fclose(file);
+//	fclose(file);
 }
