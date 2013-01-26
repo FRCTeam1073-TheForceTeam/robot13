a2bbe7e784ba2348b693fd992f92076e6e4637d0
Evin Ugur <evinoog96@gmail.com>
2013-01-26 11:03:57 -0500

    Deleted Scripts - will add as submodule

--- Scripts/GetDashboard.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-CD ..
-git clone https://github.com/FRCTeam1073-TheForceTeam/dashboard13.git ..\dashboard13
--- Scripts/GetDashboard.sh
+++
@@ -1,2 +0,0 @@
-cd ..
-git clone https://github.com/FRCTeam1073-TheForceTeam/dashboard13.git ..\dashboard13
