0295b8b3b74ed256e84593a006cc464eb1586924
Evin Ugur <evinoog96@gmail.com>
2013-02-02 10:04:38 -0500

    Data for Simulator courtesy of Mandy Graves

--- FieldElements.h
+++ FieldElements.h
@@ -1,16 +1,37 @@
-//basic (and probably temporary - best to move them to Subsystems, or maybe keep things here... who knows) field element properties 
 #ifndef FIELD_ELEMENTS_H
 #define FIELD_ELEMENTS_H
 
-#define PI = 3.14	//TODO replace this with the appropriate call in the Math library
+#define LENGTH = (54 * 12)
+#define WIDTH =  (27 * 12)
 
-//pyramid constants
-#define PYRAMID_FIRST_LEVEL_IN = 30
-#define PYRAMID_SECOND_LEVEL_IN = 60
-#define PYRAMID_THIRD_LEVEL_IN = 90
-#define PYRAMID_DIAMATER_IN = 5/PI
+#define TOP_L_PYR_Y 122 //in
+#define TOP_R_PYR_Y 122 //in
+#define TOP_L_PYR_X 61
+#define BOTTOM_PYR 216//in
 
-//target constants
-#define TAPE_WIDTH_IN = 4.5
+#define BLUE_AUTO_LINE 216
+
+#define CENTER_LINE 324
+
+//Points of Feeder Zone
+#define FEEDER_BOTTOM_X 0
+#define FEEDER_BOTTOM_Y 87.94 //in Mandy did trig, assuming it's a 45-45-90
+
+#define FEEDER_TOP_X (20 + FEEDER_BOTTOM_Y)
+#define FEEDER_TOP_Y 0
+
+#define TAPE_WIDTH 4
+
+//left backboards are the saqme as the right backboard
+#define LEFT_BACKBOARD_WIDTH 62
+
+
+#define LEFT_BACKBOARD_BASE_POINT (84.625 + TAPE_WIDTH)
+
+#define LEFT_BACK_BOARD_X (FEEDER_BOTTOM_X + 7.5 - TAPEWIDTH)	
+
+#define TOP_GOAL_X 138.72
+
+#define LINE_WIDTH 2 //thin lines
 
 #endif
