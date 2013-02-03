bdc22115a76a6d76b61dd28e0f82daeff645f4e8
Evin Ugur <evinoog96@gmail.com>
2013-02-03 14:12:35 -0500

    Complete Semiautomatic Disc Shooting Command

--- Commands/Shoot.h
+++ Commands/Shoot.h
@@ -1,18 +1,18 @@
 #ifndef SHOOT_H
 #define SHOOT_H
-
-
 #include "Commands/Subsystem.h"
 #include "../Robot.h"
-
 class Shoot: public Command {
+private:
+	bool abort;
+	enum DiscState{enteringShooter, inShooter, leftShooter};
+	DiscState discState;
 public:
 	Shoot();
 	virtual void Initialize();
 	virtual void Execute();
 	virtual bool IsFinished();
+	virtual void Interrupted() {}
 	virtual void End();
-	virtual void Interrupted();
 };
-
 #endif
