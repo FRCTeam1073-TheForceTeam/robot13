375f5e66cd630a02cc831917c21817ebc4b7ba0a
Evin Ugur <evinoog96@gmail.com>
2013-02-17 11:04:45 -0500

    Cased LW stuff to pointer to AnalogChannel

--- RobotMap.cpp
+++ RobotMap.cpp
@@ -70,11 +70,11 @@ void RobotMap::init() {
 	lw->AddActuator("Shooter", "Secondary Jag", shooterBackJag);
 	
     shooterElevationEncoder = new StallableAnalogEncoder(ANALOG_SHOOTER_ANGLE_MAG_ENCODER);
-    lw->AddSensor("Shooter", "Elevation Encoder", shooterElevationEncoder);
+    lw->AddSensor("Shooter", "Elevation Encoder", (AnalogChannel*)shooterElevationEncoder);
 	climberArmLeftWindowEncoder = new StallableAnalogEncoder(ANALOG_CLIMBER_LEFT_ANGLE_MAG_ENCODER);
-	lw->AddSensor("Climber Arms", "Left Arm Encoder", climberArmLeftWindowEncoder);
+	lw->AddSensor("Climber Arms", "Left Arm Encoder", (AnalogChannel*)climberArmLeftWindowEncoder);
 	climberArmRightWindowEncoder = new StallableAnalogEncoder(ANALOG_CLIMBER_RIGHT_ANGLE_MAG_ENCODER);
-	lw->AddSensor("Climber Arms", "Right Arm Encoder", climberArmRightWindowEncoder);
+	lw->AddSensor("Climber Arms", "Right Arm Encoder", (AnalogChannel*)climberArmRightWindowEncoder);
     velocity1 = new DigitalInput(DIGITAL_MUZZLE_VELOCITY_ONE);
     velocity2 = new DigitalInput(DIGITAL_MUZZLE_VELOCITY_TWO);
 	driveTrainGyro->Reset();
