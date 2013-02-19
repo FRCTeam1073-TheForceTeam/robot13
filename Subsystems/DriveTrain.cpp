82377eab5dc161b2aa10b034244b8395bde1429a
Evin Ugur <evinoog96@gmail.com>
2013-02-19 10:51:25 -0500

    enablded no drive on libra, removed unused debug

--- Subsystems/DriveTrain.cpp
+++ Subsystems/DriveTrain.cpp
@@ -3,7 +3,6 @@
 #include "../Commands/JoystickDrive.h"
 #include "../OI.h"
 #include "GenericHID.h"
-//#define  DEBUGDRIVETRAIN 
 #define WHEEL_SIZE 7
 #define TICKS_PER_REV 1440
 #define INCHES_PER_TICK 0.022
@@ -17,28 +16,19 @@ DriveTrain::DriveTrain() : Subsystem("DriveTrain"){
 #endif
 }   
 void DriveTrain::InitDefaultCommand() {
-	//if (Robot::GetWhichRobot() != Robot::libra)
+	if (Robot::GetWhichRobot() != Robot::libra)
 	SetDefaultCommand(new JoystickDrive());//cant drive mecanum with this code!
 }
 void DriveTrain::Move(float left, float right){ 
-#ifdef DEBUGDRIVETRAIN
-	printf("calling move\n");
-#endif
 	//printf("Left: %f Right: %f\n", left, right);
 	leftMotor->Set(left);
 	rightMotor->Set(right);
 #ifdef ADD_SECONDARY_DRIVE
 	secondaryLeftMotor->Set(left);
 	secondaryRightMotor->Set(right);
-#endif 
-#ifdef DEBUGDRIVETRAIN	
-	printf("Left Y: %f  Right Y: %f\n", left, right);
-	printf("Left M: %f  Right M: %f\n", leftMotor->Get(), rightMotor->Get());
 #endif
 	SmartDashboard::PutNumber("Right Drive Encoder", rightMotor->GetSpeed());
-	//printf("Right Motor Speed %f\n", rightMotor->GetSpeed());
 	SmartDashboard::PutNumber("Left Drive Encoder", leftMotor->GetSpeed());
-	//printf("Left Motor Speed %f\n", leftMotor->GetSpeed());
 	SmartDashboard::PutNumber("Gyro Angle", gyro->GetAngle());
 	//printf("Gyro Angle %f\n", gyro->GetAngle());
 }
