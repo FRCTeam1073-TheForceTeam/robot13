0b44d99191a45e16a47fdad80a08c0213f9de8d2
Evin Ugur <evinoog96@gmail.com>
2013-02-26 19:20:04 -0500

    these were old - deleted

--- Subsystems/Roller.h
+++ Subsystems/Roller.h
@@ -3,9 +3,6 @@
 #include "Commands/Subsystem.h"
 #include "WPILib.h"
 #include "../RobotMap.h"
-#define SPEED_FORWARD 1
-#define SPEED_REVERSE -1
-#define SPEED_OFF 0
 class Roller : public Subsystem{
 public:
 	Roller();
