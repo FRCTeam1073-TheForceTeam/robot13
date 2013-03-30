daef6d65b78acf9e724a199828af48ae903918e1
Kate <katecherian14159@gmail.com>
2013-03-30 16:48:22 -0400

    optimized collector speed -- Evin Ugur

--- Subsystems/Collector.h
+++ Subsystems/Collector.h
@@ -2,7 +2,7 @@
 #define COLLECTOR_H
 #include "Commands/Subsystem.h"
 #include "WPILib.h"
-#define COLLECTOR_SPEED_ON 0.75
+#define COLLECTOR_SPEED_ON 0.6
 #define COLLECTOR_SPEED_OFF 0.0
 class Collector: public Subsystem {
 public:
